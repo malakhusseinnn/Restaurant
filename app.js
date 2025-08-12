c77246e51086216d541a8b0e732913a0c8f2bebc
malakhusseinnn <moka8107@gmail.com>
2025-08-12 08:00:39 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+let recipes = document.querySelector(".recipes");
+
+function getRecipes(link){
+    recipes.innerHTML = "";
+    let xml = new XMLHttpRequest();
+    xml.open("GET",link,true);
+    xml.onload = () => {
+        if(xml.status == 200){
+        let response = JSON.parse(xml.responseText);
+        let data = response.recipes;
+        data.map((e)=>{
+            recipes.innerHTML += `
+                <div class = "recipe">
+                 <img src = ${e.image}>
+                 <div class = "layout"><button class = "view-recipe">View Recipe</button></div>
+                 <div class = "paragraphs">
+                 <p class = "recipe-name"> ${e.name}</p>
+                 <p class = "ingredients">Ingredients: ${e.ingredients}</p>
+                 <p class = "preparing-time">Preparing Time: <span>${e.prepTimeMinutes} min</span></p>
+                 <p class = "cooking-time">Cooking Time: <span>${e.cookTimeMinutes} min</span></p>
+                  </div>
+                </div>
+            `
+        })
+        if(data.length == 0){
+          recipes.innerHTML = `
+            <p class = "not-found">No Recipes Founded :)</p>`
+         }
+       }
+    }
+    xml.send();
+}
+
+
+let recipesLink = "https://dummyjson.com/recipes";
+
+getRecipes(recipesLink);
+
+let searchInput = document.querySelector(".search-input")
+
+searchInput.addEventListener("input" , ()=>{
+    let recipeLink = `https://dummyjson.com/recipes/search?q=${searchInput.value}`
+    getRecipes(recipeLink);
+
+})
