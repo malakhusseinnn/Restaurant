4f3f7c4872cf075c9471d13caf72f2faae2c2840
malakhusseinnn <moka8107@gmail.com>
2025-08-20 11:42:22 +0300

    View Recipes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let recipes = document.querySelector(".recipes");
 
-function getRecipes(link){
+function getRecipes(link) {
     let xml = new XMLHttpRequest();
-    xml.open("GET",link,true);
+    xml.open("GET", link, true);
     xml.onload = () => {
-        if(xml.status == 200){
-        recipes.innerHTML = "";
-        let response = JSON.parse(xml.responseText);
-        let data = response.recipes;
-        data.map((e)=>{
-            recipes.innerHTML += `
+        if (xml.status == 200) {
+            recipes.innerHTML = "";
+            let response = JSON.parse(xml.responseText);
+            let data = response.recipes;
+            data.map((e) => {
+                recipes.innerHTML += `
                 <div class = "recipe">
                  <img src = ${e.image}>
-                 <div class = "layout"><button class = "view-recipe">View Recipe</button></div>
+                 <div class = "layout"><button class = "view-recipe" data-id = '${e.id}' >View Recipe</button></div>
                  <div class = "paragraphs">
                  <p class = "recipe-name"> ${e.name}</p>
                  <p class = "ingredients">Ingredients: ${e.ingredients}</p>
@@ -21,12 +21,12 @@ function getRecipes(link){
                   </div>
                 </div>
             `
-        })
-        if(data.length == 0){
-          recipes.innerHTML = `
+            })
+            if (data.length == 0) {
+                recipes.innerHTML = `
             <p class = "not-found">No Recipes Founded :)</p>`
-         }
-       }
+            }
+        }
     }
     xml.send();
 }
@@ -38,8 +38,15 @@ getRecipes(recipesLink);
 
 let searchInput = document.querySelector(".search-input")
 
-searchInput.addEventListener("input" , ()=>{
+searchInput.addEventListener("input", () => {
     let recipeLink = `https://dummyjson.com/recipes/search?q=${searchInput.value}`
     getRecipes(recipeLink);
 
+})
+
+recipes.addEventListener("click", (e) => {
+    if (e.target.classList.contains("view-recipe")) {
+        let recipeId = e.target.dataset.id;
+        window.location.href = `recipeDetails.html?id=${recipeId}`;
+    }
 })
---
+++ recipeDetails.js
@@ -0,0 +1,33 @@
+//------------------------ Recipe Details Logic -----------------------------------------
+
+let params = new URLSearchParams(window.location.search);
+let recipeId = params.get("id");
+
+if (recipeId) {
+    let recipeImg = document.querySelector(".recipe-img");
+    let recipeName = document.querySelector(".recipe-name");
+    let recipeIngredients = document.querySelector(".recipe-ingredients");
+    let recipeInstructions = document.querySelector(".recipe-instructions");
+    let preparingTime = document.querySelector(".preparing-time");
+    let cookingTime = document.querySelector(".cooking-time");
+    let cuisine = document.querySelector(".cuising");
+
+    fetch(`https://dummyjson.com/recipes/${recipeId}`)
+        .then(res => res.json())
+        .then(data => {
+            recipeImg.innerHTML = `<img src="${data.image}" alt="${data.name}">`;
+            recipeName.textContent = data.name;
+
+            recipeIngredients.innerHTML += `<ul>${data.ingredients.map(ing => `<li>${ing}</li>`).join("")
+                }</ul>`;
+
+            recipeInstructions.innerHTML += `<ol>${data.instructions.map(step => `<li>${step}</li>`).join("")
+                }</ol>`;
+
+            preparingTime.innerHTML = `Preparing Time: <span>${data.prepTimeMinutes}</span> min`;
+            cookingTime.innerHTML = `Cooking Time: <span>${data.cookTimeMinutes}</span> min`;
+            cuisine.innerHTML = `Cuisine: <span>${data.cuisine}</span>`;
+        });
+
+}
+
